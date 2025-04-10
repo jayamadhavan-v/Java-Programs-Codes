cc8d0163273ad10e31e8244d1fbfa3549efbcf4c
jayamadhavan-v <jayamadhavan.vjm@gmail.com>
2025-04-10 22:20:02 +0530

    commmit LeapYear

--- src/LeapYear.java
+++ src/LeapYear.java
@@ -16,19 +16,6 @@ public class LeapYear {
         if (year < 1 || year > 9999) {
             return false;
         }
-
-        if (year % 4 == 0) { // if year is divisible by 4, go to next step.
-            if (year % 100 != 0) { // if year is not divisible by 100, it's a leap year, so return true.
-                return true;
-            } else { // if year was divisible by 100, let's check if it's divisible by 400.
-                if (year % 400 == 0) { // if it's divisible by 400, it's leap year.
-                    return true;
-                } else { // otherwise, it's not a leap year, so return false.
-                    return false;
-                }
-            }
-        } else { // year is not divisible by 4, so it's not a leap year.
-            return false;
-        }
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
     }
 }
